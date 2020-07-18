eb2a0d9d0af8c6785186f4e5444d90b6b3e4cf27
Victor <victor.tran@tempestas.io>
2020-07-18 14:08:44 -0400

    finish iterative sorting

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
-
+    for i, data in enumerate(arr):
+        if data == target:
+            return i
     return -1   # not found
 
 
@@ -9,6 +10,16 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
-
-
+    low = 0
+    high = len(arr)-1
+    while high >= low:
+        middle = (low + high) // 2
+        print(arr[low], arr[middle], arr[high])
+        if arr[middle] == target:
+            return middle
+        else:
+            if target < arr[middle]:
+                high = middle - 1
+            else:
+                low = middle + 1
     return -1  # not found
